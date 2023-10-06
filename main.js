f0f7722e9b51c91c99d4ec701391b77d125582ab
James-Huoth <114503449+James-Huoth@users.noreply.github.com>
2023-10-06 16:27:38 +0000

    Started adding projects, added text/image animations, and randomized shapes every time you go on the website.

--- main.js
+++ main.js
@@ -1,50 +1,88 @@
 // A minimal CSS framework: you can research
 // and choose others, or use none at all.
 //import '@picocss/pico'
+import "@fontsource/chivo"; // Defaults to weight 400
+import "@fontsource/chivo/400.css"; // Specify weight
+import "@fontsource/chivo/400-italic.css"; // Specify weight and style
+//Animation
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
 
+document.onreadystatechange = () => {
+  if (document.readyState === "complete") {
+   AOS.init()
+  }
+};
 // My attempt at a 3D object
 import * as THREE from 'three';
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 300, window.innerWidth / window.innerHeight, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 300, window.innerWidth / window.innerHeight  , 1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
-
+let choice;
 const geometry = new THREE.IcosahedronGeometry(10,0);
-const edges = new THREE.EdgesGeometry( geometry ); 
-const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) ); 
+const geometry2 = new THREE.TorusGeometry( 8, 0.25, 4, 100 );
+const geometry2clone = new THREE.TorusGeometry( 8, 0.25, 4, 100 );
+const geometry3 = new THREE.BoxGeometry( 10, 10, 10 ); 
+const geometry4 = new THREE.TorusGeometry( 8, 0.25, 4, 3 )
 
-const material = new THREE.MeshBasicMaterial( { color: 606060 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+let table = [geometry,geometry2,geometry3,geometry4]
+let num = Math.round(Math.random() * table.length)
 
-camera.position.z = 100;
+choice = table[num]
+function check(){
+
+  let table = [geometry,geometry2,geometry3,geometry4]
+let num = Math.round(Math.random() * table.length)
+
+choice = table[num]
+if(choice == null) {
+
+  check()
+}else {scene.add(choice);}
+  
+  
+}
+
+check()
+
+  camera.position.z = 30;
 camera.position.x = 0;
 camera.position.y = 0;
+
 function animate() {
-  const edges = new THREE.EdgesGeometry( geometry ); 
+  
+renderer.setSize( window.innerWidth, window.innerHeight,true );
+
+const edges = new THREE.EdgesGeometry( choice); 
 const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) ); 
-  scene.add( line );
- geometry.rotateX(0.05)
-  geometry.rotateY(0.05)
-  geometry.rotateZ(0.05)
+scene.add( line );
+ 
+ choice.rotateX(0.005)
+  choice.rotateY(0.005)
+  choice.rotateZ(0.005)
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
   scene.remove(line)
+   
 }
+
+
 animate();
 // Our stylesheet.
 import './style.css'
 
-
+/*
 // We can import raw code fragments for samples, like this:
 import fragment from './code-fragments/fragment.js?raw'
 document.querySelector('#fragment').textContent = fragment;
 
 import importExample from './code-fragments/importExample.sh?raw'
 document.querySelector('#importExample').textContent = importExample;
-
+*/
 // A code highlighter
 import hljs from 'highlight.js'
 // You can check the folder for other themes!
@@ -55,3 +93,8 @@ import 'highlight.js/styles/panda-syntax-dark.css'
 hljs.highlightAll();
 
 // This is a pretty good start for JavaScript: we will add more as needed!
+/*setTimeout(
+  ()=>document.body.style.visibility = 'visible',
+  200
+  );
+  */
