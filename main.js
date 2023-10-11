d9eb1d437e043bd66dc2dacda616dcdc9e8bec0b
James-Huoth <114503449+James-Huoth@users.noreply.github.com>
2023-10-11 13:27:08 +0000

    Added about me segment and added a chain link. Also added percentage sized text and html to scale with resolution

--- main.js
+++ main.js
@@ -28,6 +28,18 @@ const geometry2 = new THREE.TorusGeometry( 8, 0.25, 4, 100 );
 const geometry2clone = new THREE.TorusGeometry( 8, 0.25, 4, 100 );
 const geometry3 = new THREE.BoxGeometry( 10, 10, 10 ); 
 const geometry4 = new THREE.TorusGeometry( 8, 0.25, 4, 3 )
+const material = new THREE.MeshBasicMaterial( { color: 0xffffff } ); 
+const circle = new THREE.TorusGeometry( 1, 0.05, 4, 100 );
+let geometry6 = new THREE.Mesh( circle, material );
+let geometry7 = new THREE.Mesh( circle, material );
+let geometry8 = new THREE.Mesh( circle, material );
+let geometry9 = new THREE.Mesh( circle, material );
+let geometry10 = new THREE.Mesh( circle, material );
+let geometry11 = new THREE.Mesh( circle, material );
+let geometry12 = new THREE.Mesh( circle, material );
+let geometry13 = new THREE.Mesh( circle, material );
+
+
 
 let table = [geometry,geometry2,geometry3,geometry4]
 let num = Math.round(Math.random() * table.length)
@@ -49,22 +61,62 @@ if(choice == null) {
 
 check()
 
-  camera.position.z = 30;
+  camera.position.z = 80;
 camera.position.x = 0;
-camera.position.y = 0;
+camera.position.y = 30;
+
+
+
+geometry6.position.y = 20;
+geometry7.position.y = 20+1.85;
+geometry8.position.y = 20+(1.85*2);
+geometry9.position.y = 20+(1.85*3);
+geometry10.position.y = 20+(1.85*4);
+geometry11.position.y = 20+(1.85*5);
+geometry12.position.y = 20+(1.85*6);
+geometry13.position.y = 20+(1.85*7);
+
+  geometry7.rotateY(17)
+  geometry9.rotateY(17)
+  geometry11.rotateY(17)
+  geometry13.rotateY(17)
 
 function animate() {
   
-renderer.setSize( window.innerWidth, window.innerHeight,true );
+
+camera.aspect = window.innerWidth / (window.innerHeight*4);
+camera.updateProjectionMatrix();
+
+renderer.setSize( window.innerWidth, window.innerHeight *4 );
 
 const edges = new THREE.EdgesGeometry( choice); 
 const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0xffffff } ) ); 
 scene.add( line );
- 
+ scene.add(geometry6);
+ scene.add(geometry7);
+ scene.add(geometry8);
+ scene.add(geometry9);
+ scene.add(geometry10);
+ scene.add(geometry11);
+scene.add(geometry12);
+scene.add(geometry13);
+  
  choice.rotateX(0.005)
   choice.rotateY(0.005)
   choice.rotateZ(0.005)
+
+ 
+geometry6.rotateY(-0.005)
+geometry7.rotateY(-0.005)
+geometry8.rotateY(-0.005)
+geometry9.rotateY(-0.005)
+geometry10.rotateY(-0.005)
+geometry11.rotateY(-0.005)
+geometry12.rotateY(-0.005)
+geometry13.rotateY(-0.005)
+
 	requestAnimationFrame( animate );
+        camera.updateProjectionMatrix();
 	renderer.render( scene, camera );
   scene.remove(line)
    
